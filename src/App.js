8adafd35612eb5ef9590d6e303a1f3254a26e6c9
ABiteOfSorrow <laceican@naver.com>
2022-04-20 10:03:52 +0200

    Add click reaction (heart, video, stars) & rating counter

--- src/App.js
+++ src/App.js
@@ -8,55 +8,16 @@ import Movie from './components/movie';
 
 function App() {
 
-  var movieData = [{
-    name: "StarWars : L'ascension de SkyWalker",
-    img: "./img/starwars.jpg",
-    desc: "La conclusion de la saga SkyWalker. De nouvells légendes vont naître dans cette....",
-    note: 6,
-    vote: 8
-  },
-  {
-    name: "Maléfique : Le pouvoir du mal",
-    img: "./img/maleficent.jpg",
-    desc: "Plusieur années après avoir découvert pourquoi la plus célèbre méchante Disney avait un coeur....",
-    note: 9,
-    vote: 4
-  },
-  {
-    name: "Jumanji: The Next Level",
-    img: "./img/jumanji.jpg",
-    desc: "L'équipe est de retour, mais le jeu a changé. Alors qu'ils retournent dans Jumanji pour secourir....",
-    note: 7,
-    vote: 4
-  },
-  {
-    name: "Once Upon a Time...in Hollywood",
-    img: "./img/once_upon.jpg",
-    desc: "En 1969, Rick Dalton-star déclinante d'une série télévisée de zestern-et Cliff Booth....",
-    note: 3,
-    vote: 8
-  },
-  {
-    name: "La Reine des neiges 2",
-    img: "./img/frozen.jpg",
-    desc: "Elsa, Anna, Kristoff, Olaf et Sven voyagent bien au-delà des portes d'Arendelle à la recherche de réponses....",
-    note: 9,
-    vote: 8
-  },
-  {
-    name: "Terminateur: Dark Fate",
-    img: "./img/terminator.jpg",
-    desc: "De nos jours à Mexico. Dani Ramos, 21 ans, travaille sur une chaîne de montage dans une usine automobile....",
-    note: 7,
-    vote: 5
-  },
-  {
-    name: "Badboys 3 : Return to Bad Guys",
-    img: "./img/badboy3.jpg",
-    desc: "Les polices sont retourner...",
-    note: 8,
-    vote: 5
-  }
+
+
+  var movieData = [
+  {name: "StarWars : L'ascension de SkyWalker", img: "./img/starwars.jpg", desc: "La conclusion de la saga SkyWalker. De nouvells légendes vont naître dans cette....", note: 6, vote: 8},
+  {name: "Maléfique : Le pouvoir du mal", img: "./img/maleficent.jpg", desc: "Plusieur années après avoir découvert pourquoi la plus célèbre méchante Disney avait un coeur....", note: 9, vote: 4},
+  {name: "Jumanji: The Next Level", img: "./img/jumanji.jpg", desc: "L'équipe est de retour, mais le jeu a changé. Alors qu'ils retournent dans Jumanji pour secourir....", note: 7, vote: 4},
+  {name: "Once Upon a Time...in Hollywood", img: "./img/once_upon.jpg", desc: "En 1969, Rick Dalton-star déclinante d'une série télévisée de zestern-et Cliff Booth....", note: 3, vote: 8},
+  {name: "La Reine des neiges 2", img: "./img/frozen.jpg", desc: "Elsa, Anna, Kristoff, Olaf et Sven voyagent bien au-delà des portes d'Arendelle à la recherche de réponses....", note: 9, vote: 8},
+  {name: "Terminateur: Dark Fate", img: "./img/terminator.jpg", desc: "De nos jours à Mexico. Dani Ramos, 21 ans, travaille sur une chaîne de montage dans une usine automobile....", note: 7, vote: 5},
+  {name: "Badboys 3 : Return to Bad Guys", img: "./img/badboy3.jpg", desc: "Les polices sont retourner...", note: 8, vote: 5}
 ]
 
 var movieList = movieData.map((movie,i) => {
--- src/components/movie.js
+++ src/components/movie.js
@@ -1,53 +1,108 @@
 import React from 'react';
+import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, CardImg, CardBody, CardText, Button, Col, ButtonGroup, Badge} from 'reactstrap';
 import { faHeart, faVideo, faStar} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-function Movie(props) {
 
-var tabGlobalRating = [];
+function Movie(props) {
+  
+  //Moive Like Button
+  const [isMovieLike, setIsMovieLike] = useState(false);
+
+  if(isMovieLike){
+    var colorLike = {cursor:'pointer', color: '#e74c3c'};
+  } else {
+    var colorLike = {cursor:'pointer'};
+  }
+
+
+  //Moive Watch Button
+  const [watchMovie, setWatchMovie] = useState(0);
+  const [isWatchMovie, setIsWatchMovie] = useState(false);
+
+  const watchClick = () => {
+    setWatchMovie(watchMovie+1);
+    setIsWatchMovie(true)
+  }
+
+
+  //Moive rating Button
+const [value, setValue] = useState(0);
+
+const ratingPlus = () => {
+  setValue(value+1)
+  if(value >= 10){setValue(10)}
+}
+
+const ratingMinus = () => {
+  setValue(value-1)
+  if(value <= 0){setValue(0)}
+}
+
+
+  //Moive rating stars
+  var tabMyRating = [];
+  for(var i=0; i<10; i++){
+    var color={cursor:'pointer'}
+    if (i < value){
+    color = {cursor:'pointer', color:'#f1c40f'}
+   }
+  //Moive rating stars click count
+   let count = i+1
+   tabMyRating.push(<FontAwesomeIcon onClick={() => setValue(count)} style={color} icon={faStar} />)
+}
+   
+
+  //Moive rating stars average and voter count
+  var totalNote = props.globalRating * props.globalCountRating;
+      var totalVote = props.globalCountRating;
+      if(value){
+        totalNote += value;
+        totalVote += 1
+      }
+  //Moive rating stars average math Round 
+  var avgTotal = Math.round(totalNote / totalVote)
+
+  var tabGlobalRating = []
+
+  //Moive rating stars display
 for(var i=0; i<10; i++){
   var color = {}
-  if(i<props.globalRating){
+  if(i<avgTotal){
     color = {color : '#f1c40f'}
+    console.log(avgTotal, value)
   }
   tabGlobalRating.push(<FontAwesomeIcon style={color} icon={faStar} />)
 }
 
+
+
+  //Moive.js content
     return(
   <Col xs="12" lg="6" xl="4">
     <Card style={{margintBottom:30}}>
     <CardImg
       alt={props.movieName}
       src={props.movieImg}
-      width="100"
     />
     <CardBody>
       <CardText>
-      <p>LIKE <FontAwesomeIcon style={{cursor:'pointer'}} icon={faHeart} /></p>
-      <p>NOMBRE DE VUE <FontAwesomeIcon icon={faVideo} /> <Badge color="secondary">2</Badge></p>
+      <p>LIKE <FontAwesomeIcon onClick={()=>setIsMovieLike(!isMovieLike)} style={colorLike} icon={faHeart} /> <Badge color="secondary" ></Badge></p>
+      <p>NOMBRE DE VUE <FontAwesomeIcon onClick={()=>watchClick()} style={{color: isWatchMovie ? '#e74c3c' : 'secondary'}} icon={faVideo} /> <Badge color="secondary" >{watchMovie}</Badge></p>
       <p>Mon Avis
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      <FontAwesomeIcon icon={faStar}/> 
-      
+       {/* {myRatings} */}
+       {tabMyRating}
        <ButtonGroup size='sm'>
-         <Button color="secondary">-</Button>
-         <Button color="secondary">+</Button>
+         <Button onClick={()=>ratingMinus()} color="secondary">-1</Button>
+         <Button onClick={()=>ratingPlus()} color="secondary">+1</Button>
          </ButtonGroup>
          </p>
       <p>Moyenne
         {tabGlobalRating}
-        ({props.globalCountRating})</p>
+        ({totalVote})</p>
       <p>{props.movieName}</p>
       <p>{props.movieDesc}</p> 
       </CardText>
@@ -56,5 +111,9 @@ for(var i=0; i<10; i++){
 </Col>
   )
 }
+
+
+
+
 
 export default Movie;
